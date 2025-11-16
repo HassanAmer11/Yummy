d3cfa69a65df6158fd03fd7e9bce3baa301fe25d
HassanAmer11 <hassan.amer.2554@gmail.com>
2025-11-16 20:08:31 +0200

    a

--- assets/js/main.js
+++ assets/js/main.js
@@ -328,7 +328,7 @@ function showContacts() {
   });
 }
 
-function validateform(src) {
+function validateform(src, canShow = true) {
   var vaildObj = {
     emailInput:
       /(?:[a-z0-9!#$%&'*+\x2f=?^_`\x7b-\x7d~\x2d]+(?:\.[a-z0-9!#$%&'*+\x2f=?^_`\x7b-\x7d~\x2d]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9\x2d]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9\x2d]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9\x2d]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/,
@@ -342,7 +342,7 @@ function validateform(src) {
     hideErrors(src);
     return true;
   } else {
-    showErrors(src);
+    canShow ? showErrors(src) : null;
     return false;
   }
 }
@@ -359,12 +359,12 @@ function showErrors(src) {
   src.classList.add("is-invalid");
 }
 
-function checkEquality(src) {
+function checkEquality(src, canShow = true) {
   if (src.value == document.getElementById("passwordInput").value) {
     hideErrors(src);
     return true;
   } else {
-    showErrors(src);
+    canShow ? showErrors(src) : null;
     return false;
   }
 }
@@ -372,12 +372,12 @@ function checkEquality(src) {
 function enableSubmitBtn(flag) {
   if (
     flag &&
-    validateform(nameInput) &&
-    validateform(emailInput) &&
-    validateform(ageInput) &&
-    validateform(phoneInput) &&
-    validateform(passwordInput) &&
-    checkEquality(repasswordInput)
+    validateform(nameInput, false) &&
+    validateform(emailInput, false) &&
+    validateform(ageInput, false) &&
+    validateform(phoneInput, false) &&
+    validateform(passwordInput, false) &&
+    checkEquality(repasswordInput, false)
   ) {
     submitBtn.removeAttribute("disabled");
   } else {
